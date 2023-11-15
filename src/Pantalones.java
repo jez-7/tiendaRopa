2626ca3189110ed4f9ac29f65d5e71bec3c12478
emanuel zamora <137125466+jez-7@users.noreply.github.com>
2023-11-15 12:45:25 -0300

    Delete src/Pantalones.java

--- src/Pantalones.java
+++
@@ -1,6 +0,0 @@
-public class Pantalones extends productos {
-
-    public Pantalones(String color, int id, String nombre, int precio, String talle) {
-        super(color, id, nombre, precio, talle);
-    }
-}
